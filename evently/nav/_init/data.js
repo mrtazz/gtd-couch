9207f684e34a4e13205c343aa4791aba7e43437e
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-18 20:30:42 +0200

    use projects_and_context view in navigation

--- evently/nav/_init/data.js
+++ evently/nav/_init/data.js
@@ -1,5 +1,20 @@
-function(e) {
-  return {"projects": [{"name": "project1"}],
-                            "contexts": [{"name": "context1"}]
-                           };
+function(data) {
+
+  var projects = [];
+  var contexts = [];
+
+  // parse the result and divide in contexts and projects
+  for (r in data.rows)
+  {
+    if (data.rows[r].value.type == "context")
+    {
+      contexts.push(data.rows[r].value);
+    }
+    else
+    {
+      projects.push(data.rows[r].value);
+    }
+  }
+
+  return {"projects": projects, "contexts": contexts };
 }
