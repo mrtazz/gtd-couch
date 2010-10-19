e8bbb70c51640b348a3afa1c1d7581796c79b2e5
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-19 11:22:53 +0200

    add submit function for newentry

--- evently/newentry/_init/selectors/form/submit.js
+++ evently/newentry/_init/selectors/form/submit.js
@@ -1,4 +1,21 @@
 function() {
-  alert("You clicked submit!");
+  var form = $(this),
+    f = form.serializeObject(),
+    doc = {
+      created_at : new Date(),
+      owner : $$("#profile").profile,
+      title : f.title,
+      notes : f.notes,
+      tags : f.tags.split(","),
+      duedate : f.duedate,
+      project : f.project,
+      type : "task"
+    };
+  $$(form).app.db.saveDoc(doc, {
+    success : function(r) {
+      $.log("Success!");
+    }
+  });
+
   return false;
 };
