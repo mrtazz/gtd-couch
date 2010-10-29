9bee0a1a7bab5043f86772e5801989ce255c6b0b
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-29 12:27:47 +0200

    update task status via checkbox

---
+++ evently/tasks/loggedIn/selectors/input/change.js
@@ -0,0 +1,21 @@
+function()
+{
+  var app = $$(this).app;
+  var box = this;
+  var docid = this.parentNode.getAttribute("docid");
+  app.db.openDoc(docid, {
+      success: function(d)
+      {
+        if(box.checked)
+        {
+          d["status"] = "done";
+        }
+        else
+        {
+          d["status"] = "wip";
+        }
+        app.db.saveDoc(d, {success: function(r){}});
+      }
+  });
+  $.log(doc);
+}
