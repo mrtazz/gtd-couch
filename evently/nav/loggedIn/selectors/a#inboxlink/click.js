2e373efd5456eadbca7609fd019af5c26c54a5bd
Daniel Schauenberg <d@unwiredcouch.com>
2010-12-27 15:22:26 +0100

    fill task edit box with correct projects/contexts

--- evently/nav/loggedIn/selectors/a#inboxlink/click.js
+++ evently/nav/loggedIn/selectors/a#inboxlink/click.js
@@ -1,11 +1,31 @@
 function(){
-  // pass data to setTasks event
-  $$(this).app.db.view("couchapp/inbox", {
-    success: function(resp)
-    {
-      $("#tasks").trigger("setTasks", resp);
-    }
+
+  // set app object
+  var app = $$(this).app;
+  /**
+   * @brief method to take existing data and concatenate them onto tasks
+   *
+   * @param data obj containing rows to concatenate
+   *
+   */
+  var query_tasks = function(data)
+  {
+    app.db.view("couchapp/inbox", {
+      success: function(resp)
+      {
+        // combine data
+        resp.rows.push.apply(resp.rows, data.rows);
+        $("#tasks").trigger("setTasks", resp);
+      }
+    });
+  };
+
+  // query projects and contexts and then call query_tasks
+  app.db.view("couchapp/projects_and_contexts", {
+    success: query_tasks
   });
+
+  // setup activity indicators
   $("#navul a").removeClass("active");
   $(this).addClass("active");
 }
