8742bf3af309215ccb1889da92a08c165f57fab4
Daniel Schauenberg <d@unwiredcouch.com>
2010-12-28 13:49:43 +0100

    implement "completed_at" tagging

--- evently/tasks/setTasks/selectors/input.checkdone/change.js
+++ evently/tasks/setTasks/selectors/input.checkdone/change.js
@@ -4,17 +4,21 @@ function()
   var box = this;
   var docid = this.parentNode.getAttribute("docid");
   app.db.openDoc(docid, {
-      success: function(d)
+      success: function(doc)
       {
         if(box.checked)
         {
-          d["status"] = "done";
+          var d = new Date();
+          var today = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+          doc["completed_at"] = today;
+          doc["status"] = "done";
         }
         else
         {
-          d["status"] = "wip";
+          doc["status"] = "wip";
+          doc["completed_at"] = "";
         }
-        app.db.saveDoc(d, {
+        app.db.saveDoc(doc, {
           success: function(r)
           {
             $('li[docid|="'+docid+'"]').slideUp("fast");
